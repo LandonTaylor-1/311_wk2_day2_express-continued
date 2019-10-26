e43c865b993bff8b31aa604837ccb8607b52b14c
Landon Taylor <landon.autumn.taylor@gmail.com>
2019-10-26 15:24:48 -0500

    Landon Taylor

--- index.js
+++ index.js
@@ -2,6 +2,44 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
+let comments = require("./data/comments");
+let contacts = require("./data/contacts");
+let products = require("./data/products");
+let vehicles = require("./data/vehicles");
+
+app.get('/comments', (req,res)=>{
+    return res.json(comments);
+});
+app.get('/contacts', (req,res)=>{
+    return res.json(contacts);
+});
+app.get('/products', (req,res)=>{
+    return res.json(products);
+});
+app.get('/vehicles', (req,res)=>{
+    return res.json(vehicles);
+});
+
+app.get('/comments/:id', (req,res)=>{
+    let comment = comments.find(e => e._id == req.params.id)
+    return res.json(comment);
+});
+app.get('/contacts/:id', (req,res)=>{
+    let contact = contacts.find(e => e._id == req.params.id)
+    return res.json(contact);
+});
+app.get('/products/:id', (req,res)=>{
+    let product = products.find(e => e._id == req.params.id)
+    return res.json(product);
+});
+app.get('/vehicles/:id', (req,res)=>{
+    let vehicle = vehicles.find(e => e._id == req.params.id)
+    return res.json(vehicle);
+});
+
+app.use(express.static("public"));
+app.use(bodyParser.json());
+
 const port = process.env.PORT || 4001;
 
 app.listen(port, () => {
